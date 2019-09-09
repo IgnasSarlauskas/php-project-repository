dd409398356ce63416a15dc245332b4b0b37b6d2
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-09 11:25:23 +0300

    1 uzduotis 09/09

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+print 'laols';
+
+?>
+
+<html>
+    <head>
+        <title>As valdau HTML</title>
+    </head>
+    <body>
+        <h1>Ignas HTML, asas!</h1>
+        <p>Moku kurti statini turini</p>
+    </body>
+</html>
