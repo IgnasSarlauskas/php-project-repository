1064368f5c820c02cd776e2d65c3c03e3862420e
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-10 13:00:22 +0300

    09/10 #1 print fix

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     </head>
     <body>
         <h1>
-            Ignas - HTML <?php print ' ir PHP' ?> asas! jau nuo <?php print date('Y') . ' metu';?>
+            Ignas - HTML <?php print ' ir PHP' ?> asas jau nuo <?php print date('Y') . ' metu';?>
         </h1>
         <p>
-            Viskas prasidejo <?php print date('d') . ' diena!' . print date('m') . ' menesio, ' ; ?> 
+            Viskas prasidejo <?php print date('m') . ' menesio' . date(' d') . ' diena! ' ; ?> 
         </p>
     </body>
 </html>
