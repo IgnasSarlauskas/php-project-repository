d3023ded48e9f9030f773a2600dd857b1062bcc2
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-10 12:32:54 +0300

    09/10 #1 uzduotis

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>As valdau HTML <?php print ' ir PHP' ?></title>
+        <title><?php print'As' . date('l') . ' ir PHP'; ?></title>
     </head>
     <body>
-        <h1>Ignas HTML <?php print ' ir PHP' ?> asas!</h1>
-        <p>Kuriu dinamini turini<p>
-        <p><?php print date("Y/m/d"); ?> <?php print date("h:i:sa"); ?>
+        <h1>
+            Ignas - HTML <?php print ' ir PHP' ?> asas! jau nuo <?php print date('Y') . ' metu';?>
+        </h1>
+        <p>
+            Viskas prasidejo <?php print date('d') . ' diena!' . print date('m') . ' menesio, ' ; ?> 
         </p>
     </body>
 </html>
