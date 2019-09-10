8e3725c5487b3d0564601a85d7bdcd1b5a91b31d
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-10 16:32:02 +0300

    strtotime() php function

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<?php
-//sdfghjk
-//zsdfghj
-//zsdfghjwertgyhujk
-
-
-/** 
- * si eilute pasisveikina
- */
-
-print 'labas';
-// print 'iki';
-
+<?php 
+    date_default_timezone_set("Europe/Vilnius");
+//    print date('Y-m-d', strtotime('+1 week'));
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title><?php print'As' . date(' l') . ' ir PHP'; ?></title>
+        <title><?php print' PHP lydes ir ' . date('Y-m-d', strtotime('+1 day')); ?></title>
     </head>
     <body>
         <h1>
-            Ignas - HTML <?php print ' ir PHP' ?> asas jau nuo <?php print date('Y') . ' metu';?>
+            Ignas - PHP su manimi buvo ir <?php print date('H:i:s', strtotime('-1 hour')); ?> 
         </h1>
         <p>
-            Viskas prasidejo <?php print date('m') . ' menesio' . date(' d') . ' diena! ' ; ?> 
+           <?php print date('Y', strtotime('+1 year ')); ?> ne uz kalnu !
         </p>
+        
     </body>
 </html>
