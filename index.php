51f8533be6b355ee0bf69a2d274d0692be6ea819
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-11 12:07:27 +0300

    09/11 #8 uzduotis: inline css & PHP

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php 
-    date_default_timezone_set("Europe/Vilnius");
+
+date_default_timezone_set("Europe/Vilnius");
+
+$h1_font = rand(5,32);
+$body_rgb = rand(0,255) . ', ' . rand(0,255) . ', ' . rand(0,255);
+$p_rgb = rand(0,255) . ', ' . rand(0,255) . ', ' . rand(0,255);
+    
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> PHP lydes ir <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'years')); ?></title>
+        <title>PHP lydes ir <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'years')); ?></title>
     </head>
-    <body>
-        <h1>
+    <body style="background-color:rgb(<?php print $body_rgb?>);">
+        <h1 style="font-size:<?php print $h1_font;?>px;">
             Ignas - galbut turesiu <?php print rand(1, 5); ?> vaiką(us)!
         </h1>
-        <p>
-           D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . ' year ')); ?>
+        <p style="color:rgb(<?php print $p_rgb?>);">
+           D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'years')); ?>
         </p>
         
     </body>
