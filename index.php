dd98fc59b03e5869423bb8efb0bb15f52be76fe6
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 14:18:58 +0300

    09/12 #4 uzduotis. siuksliu kibirs fix round() to floor()

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $siuksliu_turis_per_d = 15;
 $max_kaupo_turis = rand(0, 15);
 
 $bendras_turis = $siukslines_turis + $max_kaupo_turis;
-$dienu_skaicius = round($bendras_turis / $siuksliu_turis_per_d);
+$dienu_skaicius = floor($bendras_turis / $siuksliu_turis_per_d);
 
 
 print 'Po ' . $dienu_skaicius . 'dienu ' . date('Y-m-d', strtotime('+' . $dienu_skaicius . 'day')) . ' pirk geliu ir sampano, jei nori isvengti knflikto '
