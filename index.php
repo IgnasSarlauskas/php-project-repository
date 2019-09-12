615f320d3415489d16f455137ae4e8b9c062ed04
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 10:19:29 +0300

    09/12 #2 img fix

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-$image = "https://i.ytimg.com/vi/zw0pF1sfnsA/hqdefault.jpg";
-?>
+$image = 'https://i.ytimg.com/vi/zw0pF1sfnsA/hqdefault.jpg';
 
+?>
 <html>
     <head>
         <meta charset="UTF-8">
     </head>
     <body>
-        <div><img src = "<?php print $image; ?>"</div>
-        <div><img src = "<?php print $image; ?>"</div>
-        <div><img src = "<?php print $image; ?>"</div>
-        <div><img src = "<?php print $image; ?>"</div>
+        <img src="<?php print $image; ?>">
+        <img src="<?php print $image; ?>">
+        <img src="<?php print $image; ?>">
+        <img src="<?php print $image; ?>">
     </body>
 </html>
