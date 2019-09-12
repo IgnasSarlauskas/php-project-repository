cfd495bfb8a69795d2f7523caefd815830d41020
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 11:10:38 +0300

    #3 clean code shoot four

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$debt_one = rand(101, 200);
-$debt_two = rand(201, 300);
-$debt_three = rand(301, 400);
+$debt_not_acceptable = rand(101, 200);
+$debt = rand(201, 300);
+$debt_acceptable = rand(301, 400);
 
 ?>
 <html>
@@ -12,9 +12,9 @@ $debt_three = rand(301, 400);
     <body>
         <div>
             <h1>Skolos skaičiuoklė</h1>
-            <h3>Jei paėmei <?php print $debt_two; ?>eurų</h3>
-            <h3>Su dviem kabančiais grąžinsi <?php print $debt_three; ?>eurų</h3>
-            <h3>Su vienu kabančiu grąžinsi <?php print $debt_one; ?>eurų</h3>
+            <h3>Jei paėmei <?php print $debt; ?>eurų</h3>
+            <h3>Su dviem kabančiais grąžinsi <?php print $debt_acceptable; ?>eurų</h3>
+            <h3>Su vienu kabančiu grąžinsi <?php print $debt_not_acceptable; ?>eurų</h3>
         </div>
     </body>
 </html>
