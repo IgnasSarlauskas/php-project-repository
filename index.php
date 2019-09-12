c1b3f48c9d7314fd6daacd8622399a1fbb297c63
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 10:49:42 +0300

    09/12 #3 clean code task

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-$image = 'https://i.ytimg.com/vi/zw0pF1sfnsA/hqdefault.jpg';
+$debt_one = (rand(1, 100));
+$debt_two = (rand(101, 200));
+$debt_three = (rand(201, 300));
+$debt_four = (rand(301, 400));
 
 ?>
 <html>
@@ -8,9 +11,11 @@ $image = 'https://i.ytimg.com/vi/zw0pF1sfnsA/hqdefault.jpg';
         <meta charset="UTF-8">
     </head>
     <body>
-        <img src="<?php print $image; ?>">
-        <img src="<?php print $image; ?>">
-        <img src="<?php print $image; ?>">
-        <img src="<?php print $image; ?>">
+        <div class="debt-section">
+            <h1>Skolos skaičiuoklė</h1>
+            <h3>Jei paėmei <?php print $debt_three; ?> eurų</h3>
+            <h3>Su dviem kabančiais grąžinsi <?php print $debt_four; ?> eurų</h3>
+            <h3>Su vienu kabančiu grąžinsi <?php print $debt_two; ?> eurų</h3>
+        </div>
     </body>
 </html>
