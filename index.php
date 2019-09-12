7ccd8b4bb5c429401c20f73fb83db4558bb29263
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 10:57:50 +0300

    09/12 #3 clean code task second shot

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-$debt_one = (rand(1, 100));
-$debt_two = (rand(101, 200));
-$debt_three = (rand(201, 300));
-$debt_four = (rand(301, 400));
+$debt_one = (rand(101, 200));
+$debt_two = (rand(201, 300));
+$debt_three = (rand(301, 400));
 
 ?>
 <html>
@@ -11,11 +10,11 @@ $debt_four = (rand(301, 400));
         <meta charset="UTF-8">
     </head>
     <body>
-        <div class="debt-section">
+        <div>
             <h1>Skolos skaičiuoklė</h1>
-            <h3>Jei paėmei <?php print $debt_three; ?> eurų</h3>
-            <h3>Su dviem kabančiais grąžinsi <?php print $debt_four; ?> eurų</h3>
-            <h3>Su vienu kabančiu grąžinsi <?php print $debt_two; ?> eurų</h3>
+            <h3>Jei paėmei <?php print $debt_two; ?>eurų</h3>
+            <h3>Su dviem kabančiais grąžinsi <?php print $debt_three; ?>eurų</h3>
+            <h3>Su vienu kabančiu grąžinsi <?php print $debt_one; ?>eurų</h3>
         </div>
     </body>
 </html>
