ead73d3c51bdab70fc87b9086e39c505c5fa195c
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-12 12:42:06 +0300

    09/12 #4 uzduotis. siuksliu kibirs fix

--- index.php
+++ index.php
@@ -2,27 +2,13 @@
 
 $siukslines_turis = 40;
 $siuksliu_turis_per_d = 15;
-$max_kaupo_turis = rand(0,15);
+$max_kaupo_turis = rand(0, 15);
 
-        
-$bendras_turis =  $siukslines_turis + $max_kaupo_turis;
+$bendras_turis = $siukslines_turis + $max_kaupo_turis;
 $dienu_skaicius = round($bendras_turis / $siuksliu_turis_per_d);
 
 
-
-print 'Po ' . $dienu_skaicius . 'dienu ' .  date('Y-m-d', strtotime('+' . $dienu_skaicius . 'day')) . ' pirk geliu ir sampano, jei nori isvengti knflikto '
-
-
-
-
+print 'Po ' . $dienu_skaicius . 'dienu ' . date('Y-m-d', strtotime('+' . $dienu_skaicius . 'day')) . ' pirk geliu ir sampano, jei nori isvengti knflikto '
         
-
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        
-    </body>
-</html>
+
