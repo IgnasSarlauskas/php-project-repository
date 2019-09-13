20d5080b958656503434c5d46835253b5427dbb3
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-13 12:10:06 +0300

    #4 uzduotis. ar miegosi ant sofos

--- index.php
+++ index.php
@@ -7,12 +7,16 @@ $h1_text = 'Buitine Skaiciuokle';
 
 if ($grizai_velai && !$grizai_isgeres ) {
     $text_outcome = 'Situacija: Grizai velai';
-} elseif ($grizai_velai && !$grizai_isgeres) {
+    $text_outcome_2 = 'Isvada: Nemiegosi ant sofos';
+} elseif ($grizai_velai && $grizai_isgeres) {
     $text_outcome = 'Situacija: Grizai velai ir isgeres';
+    $text_outcome_2 = 'Isvada: Miegosi ant sofos';
 } elseif ($grizai_isgeres && !$grizai_velai) {
     $text_outcome = 'Situacija: Grizai isgeres';
+    $text_outcome_2 = 'Isvada: Nemiegosi ant sofos';
 } else {
     $text_outcome = 'Situacija: Nieko nepadarei';
+    $text_outcome_2 = 'Isvada: Nemiegosi ant sofos';
 }
 
 ?>
@@ -23,5 +27,6 @@ if ($grizai_velai && !$grizai_isgeres ) {
     <body>
         <h1><?php print $h1_text; ?></h1>
         <h2><?php print $text_outcome; ?></h2>
+        <h3><?php print $text_outcome_2; ?></h3>
     </body>
 </html>
