73ad5e58f413ba5dad50817786b4f02595b3642c
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-13 10:30:36 +0300

    09/13 #2 uzduotis fuel & trip + if else logic if you have cash cash money money $ biatch

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <?php
 
-$distance = rand(50, 100);
+$distance = rand(100, 500);
 $consumption = 7.5;
-$price_l = 1.3;
+$price_1 = 1.3;
+$my_money = 20;
 
-$fuel_total = ($distance * $consumption) / 100 ;
-$price_trip = $fuel_total * $price_l;
+$fuel_total = $distance * $consumption / 100;
+$price_trip = $fuel_total * $price_1;
 
-$title_text = 'Keliones skaiciuokle ';
-$distance_text = 'Nuvaziuota distancija: ';
-$consumption_text_start = 'Sunaudota: ';
-$consumption_text_end = ' l. kuro.';
-$price_text_start = 'Sunaudota: ';
-$price_text_end = ' pinigu';
+$title = 'Kelionės skaičiuoklė';
+$text_1 = "Nuvažiuota distancija: $distance";
+$text_2 = "Sunaudota $fuel_total l. kuro";
+$text_3 = "Kaina: $price_trip pinigų";
+$text_4 = "Turimi pinigai: $my_money $";
+
+
+if ($my_money >= $price_trip) {
+    $text_p = 'Isvada: Kelione iperkama';
+} else {
+    $text_p = 'Isvada: Kelione neiperkama';
+}
 
 ?>
 <html>
     <head>
-        <meta charset="UTF-8" />
+        
     </head>
-    <body>
-        <h1><?php print $title_text; ?></h1>
+     <body>
         <ul>
-            <li><?php print $distance_text . $distance; ?></li>
-            <li><?php print $consumption_text_start . $fuel_total . $consumption_text_end; ?></li>
-            <li><?php print $price_text_start . $price_trip . $price_text_end; ?></li>
+            <li><?php print $text_1; ?></li>
+            <li><?php print $text_2; ?></li>
+            <li><?php print $text_3; ?></li>
+            <li><?php print $text_4; ?></li>
+            <hr>
+            <p><?php print $text_p; ?></p>
         </ul>
     </body>
 </html>
-
