fbfdccc58919c11900b34e3c68d9a2f8e115598b
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-16 09:42:07 +0300

    09/16 #1 For cycle

--- index.php
+++ index.php
@@ -1,44 +1,9 @@
 <?php
 
-$sunny = rand(0, 1);
+$x = rand(0, 99);
 
-if ($sunny) {
-    $img = 'sunny';
-    $text = 'sauleta';
-} else {
-    $img = 'rainy';
-    $text = 'debesuota';
+for ($x = 0; $x < rand(0, 50); $x++) {
+    print 'Tai yra ' . $x . '-tasis ciklas';
 }
 
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>Klases darbas</title>
-    </head>
-    <style>
-        .sunny {
-            background-image :url(http://www.cashadvance6online.com/data/archive/img/2313238826.png);
-            background-size: cover;
-            height: 500px;
-            width: 500px;
-        }
-
-        .rainy {
-            background-image :url(http://s13.favim.com/orig/160903/book-books-cloud-cloudy-Favim.com-4697653.jpeg);
-            background-size: cover;
-            height: 500px;
-            width: 500px;
-        }
-
-        p {
-            display: inline-block;
-            position: relative; 
-            bottom: 250px;
-        }
-    </style>
-    <body>
-        <img class ="<?php print $img; ?>">
-        <p><?php print $text; ?></p>
-    </body>
-</html>
