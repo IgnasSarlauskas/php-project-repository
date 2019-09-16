d40f70026a909b378e98d4bc8ee645bd6c375c08
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-16 15:58:29 +0300

    09/16 #7 HomeWork Time Spent smoking fix

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $days = 365;
 $pack_price = 3.5;
 $count_ttl = 0;
+
 $time_per_cig = 300;
 
 for ($i = 0; $i < $days; $i++) {
@@ -21,16 +22,18 @@ for ($i = 0; $i < $days; $i++) {
     }
 }
 
+// #5 task part:
+$price_ttl = $count_ttl / 20 * $pack_price;
+$h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+
+// #7 task part
 $time_in_seconds = $count_ttl * $time_per_cig;
-
 //$time_total = gmdate("H:i:s", $time_in_seconds);
-
 $seconds = $time_in_seconds ;
 $hours = floor($seconds / 3600);
 $seconds -= $hours * 3600;
 $minutes = floor($seconds / 60);
 $seconds -= $minutes * 60;
-
 $time_total = "$hours:$minutes:$seconds";
 
 $h3_text = "Is viso traukdamas prastovesiu $time_total valandu";
@@ -42,6 +45,7 @@ $h3_text = "Is viso traukdamas prastovesiu $time_total valandu";
     </head>
     <body>
         <h1>Dumu skaiciuokle</h1>
-        <h2><?php print $h3_text; ?></h2>
+        <h2><?php print $h2_text; ?></h2>
+        <h3><?php print $h3_text; ?></h3>
     </body>
 </html>
