b7a24c356d7f2ea3f8472d5ef33cc894e8c1e810
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-16 16:53:10 +0300

    09/16 HomeWork H1 print picture for every smoked ciggie

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ for ($i = 0; $i < $days; $i++) {
     }
 }
 
+
 // #5 task part:
 $price_ttl = $count_ttl / 20 * $pack_price;
 $h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
@@ -29,7 +30,7 @@ $h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl e
 // #7 task part
 $time_in_seconds = $count_ttl * $time_per_cig;
 //$time_total = gmdate("H:i:s", $time_in_seconds);
-$seconds = $time_in_seconds ;
+$seconds = $time_in_seconds;
 $hours = floor($seconds / 3600);
 $seconds -= $hours * 3600;
 $minutes = floor($seconds / 60);
@@ -43,9 +44,26 @@ $h3_text = "Is viso traukdamas prastovesiu $time_total valandu";
     <head>
         <meta charset="utf-8">
     </head>
+    <style>
+        .img {
+            background-image: url(https://fizzynora.com/102/cigarette_png/cigarette_png_97.png);
+            background-size: cover;
+            height: 40px;
+            width: 40px;
+        }
+
+        div {
+            display: inline-block;
+        }
+    </style>
     <body>
         <h1>Dumu skaiciuokle</h1>
         <h2><?php print $h2_text; ?></h2>
-        <h3><?php print $h3_text; ?></h3>
+        <h3><?php print $h3_text; ?></h3> 
+        <div class="container">
+            <?php for ($x = 0; $x < $count_ttl; $x++): ?>
+                <img class="img"/>
+            <?php endfor; ?>
+        </div>
     </body>
 </html>
