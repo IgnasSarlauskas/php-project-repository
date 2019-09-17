acf86e123003476205e25bd8e8b8fb7e6aaaf548
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-17 12:32:51 +0300

    09/17 #2 car price value

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <?php
 
-$grikiai = 5000;
-$days = 0;
-$grikiai_start = $grikiai;
+$months = 24;
+$car_price_new = 30000;
+$car_price_used = $car_price_new;
+$depreciation = 2; //% per month
 
-for ($per_day = rand(200, 500); $grikiai > 0; $days++) {
-    if ($grikiai < $per_day) {
-        break;
-    } else {
-        $grikiai -= $per_day;
-    }
+for ($i = 0; $i < $months; $i++) {
+
+    $car_price_used -= ($car_price_used / 100 * $depreciation);
+   
+    
 }
 
-$end_date = date('Y-m-d', strtotime(" + $days  days"));
-$h2_text = "Rasta grikiu: $grikiai_start g.";
-$h3_text = "Isgyvensiu dar $days dienas, iki $end_date";
+$depr_perc = round($car_price_used * 100 / $car_price_new);
+$car_price_used = round($car_price_used);
 
+$h1_text = 'Kiek nuvertes masina?';
+$h2_text = "Naujos masinos kaina: $car_price_new";
+$h3_text = "Po $months men, masinos verte bus: $car_price_used eur.";
+$h4_text = "Masina nuvertes $depr_perc proc.";
 ?>
+
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
-        <h1>Kiek dienu galesi valgyti grikius</h1>
+        <h1><?php print $h1_text; ?></h1>
         <h2><?php print $h2_text; ?></h2>
         <h3><?php print $h3_text; ?></h3> 
+        <h4><?php print $h4_text; ?></h4>
     </body>
 </html>
