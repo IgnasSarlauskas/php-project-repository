3271b2d8e64745630bf80d9b0affc3955dcd331d
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-18 12:13:29 +0300

    09/18 #1 foreach clean code fix

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 
-$my_memory = ['Penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis',];
+$my_memory = ['Penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis']
         
 ?>
-
 <html>
     <head></head>
     <body>
         <h1>Kas buvo penktadieni?!</h1>
         <h2>Igno atmintis</h2>
         <ul>
-            <?php foreach ($my_memory as $value) : ?>
+            <?php foreach ($my_memory as $value): ?>
                 <li><?php print $value; ?></li>
             <?php endforeach; ?>
         </ul>
