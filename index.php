525163eb2143ee8231a4a4372b425f094a512f4d
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-18 15:54:01 +0300

    09/18 homework array palyginimas

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-$friend_memory = ['penktadienis', 'paskaita', 'namai', 'skambutis', 'draugeliui', '$alus++', '......','galvos skausmas', 'pirmadienis']
-        
+$friend_memory = ['penktadienis', 'paskaita', 'namai', 'skambutis', 'draugeliui', '$alus++', '......', 'galvos skausmas', 'sekmadienis'];
+$my_memory = ['penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis'];
+
+$same_memories[] = array_intersect($friend_memory, $my_memory);
+
 ?>
 <html>
     <head></head>
     <body>
         <h1>Kas buvo penktadieni?!</h1>
-        <h2>Draugo atmintis</h2>
+        <h2>Sutape prisiminimai</h2>
         <ul>
-            <?php foreach ($friend_memory as $value): ?>
-                <li><?php print $value; ?></li>
+            <?php foreach ($same_memories as $index): ?>
+                <?php foreach ($index as $value): ?>
+                    <li><?php print $value; ?></li>
+                <?php endforeach; ?>
             <?php endforeach; ?>
         </ul>
     </body>
