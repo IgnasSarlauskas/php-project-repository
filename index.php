8f5fdb2104e5e4951a2fba4f52814a402b78bd31
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-18 13:06:42 +0300

    09/18 #2 rand() count() random memory

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
+$my_memory = ['penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis'];
 
-$my_memory = ['penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis']
-        
+$memories_count = count($my_memory) - 1;
+$rand_flashback = rand(0, $memories_count);
+
+$h1_text = 'Kas buvo Pentkadienį?';
+$h2_text = 'Igno atmintis: ';
+$flashback_text = "Index $rand_flashback : $my_memory[$rand_flashback]";
+
 ?>
 <html>
-    <head></head>
     <body>
-        <h1>Kas buvo penktadieni?!</h1>
-        <h2>Igno atmintis</h2>
+        <h1><?php print $h1_text; ?></h1>
+        <h2><?php print $h2_text; ?>
         <ul>
             <?php foreach ($my_memory as $value): ?>
                 <li><?php print $value; ?></li>
-            <?php endforeach; ?>
+            <?php endforeach; ?>   
         </ul>
-    </body>
+        <h3><?php print $flashback_text; ?></h3>
+</body>
 </html>
