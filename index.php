20c14e4bd4de1535970e7681b994f80c61eff1fe
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-18 14:31:03 +0300

    09/18 Konsultacijos su Aurimu #1 for su savaites dienom

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
-$my_memory = ['penktadienis', 'paskaita', 'namai', 'zvanokas', 'draugeliui', '$alus++', '......', 'pirmadienis'];
 
-$memories_count = count($my_memory) - 1;
-$rand_flashback = rand(0, $memories_count);
+$array = [];
 
-$h1_text = 'Kas buvo Pentkadienį?';
-$h2_text = 'Igno atmintis: ';
-$flashback_text = "Index $rand_flashback : $my_memory[$rand_flashback]";
+for ($i = 0; $i < 7; $i++) {
+    $array[] = date('l', strtotime("+ $i days"));
+}
+var_dump($array);
 
-?>
-<html>
-    <body>
-        <h1><?php print $h1_text; ?></h1>
-        <h2><?php print $h2_text; ?>
-        <ul>
-            <?php foreach ($my_memory as $value): ?>
-                <li><?php print $value; ?></li>
-            <?php endforeach; ?>   
-        </ul>
-        <h3><?php print $flashback_text; ?></h3>
-</body>
-</html>
+
