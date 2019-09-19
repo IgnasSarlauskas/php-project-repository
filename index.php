dc84f48426fd42705de136c77c37110a52b07270
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-19 15:34:44 +0300

    09/19 HomeWork #3 Balance/Expenses/Income

--- index.php
+++ index.php
@@ -19,14 +19,25 @@ $bank_report = [
     ]
 ];
 
+$balance = 0;
+$total_income = 0;
+$total_expenses = 0;
+
 foreach ($bank_report as $transaction_key => $transaction) {
     if ($bank_report[$transaction_key]['amount'] < 0) {
         $bank_report[$transaction_key]['css_class'] = 'expense';
+        $total_expenses += $bank_report[$transaction_key]['amount'];
     } else {
         $bank_report[$transaction_key]['css_class'] = 'income';
+        $total_income += $bank_report[$transaction_key]['amount'];
     }
+
+    $balance += $bank_report[$transaction_key]['amount'];
 }
 
+$balance_text = "Balansas: $balance eur.";
+$total_income_text = "Iplaukos: $total_income eur.";
+$total_expenses_text = "Islaidos: $total_expenses eur.";
 ?>
 
 <html>
@@ -44,15 +55,22 @@ foreach ($bank_report as $transaction_key => $transaction) {
     <body>
         <h1>Banko ataskaita</h1>
         <ul>
-            <?php foreach ($bank_report as  $transaction): ?>
+            <?php foreach ($bank_report as $transaction): ?>
                 <li class="<?php print $transaction['css_class']; ?>">
                     <?php print $transaction['name']; ?>: 
                     <?php print $transaction['amount']; ?> eur
                 </li>
-            <?php endforeach; ?>
+             <?php endforeach; ?>
         </ul>
+        <h2><?php print $balance_text; ?></h2>
+        <h3><?php print $total_income_text; ?></h3>
+        <h3><?php print $total_expenses_text; ?></h3>
+        <h3></h3>
     </body>
 </html>
+
+
+
 
 
 
