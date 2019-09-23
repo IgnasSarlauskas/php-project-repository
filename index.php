55a0fd85138f28b4dd17e2ebd7653ae718755dec
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-23 14:20:53 +0300

    pizdamato termometras clean code

--- index.php
+++ index.php
@@ -1,52 +1,156 @@
 <?php
 
-$rankinukas = [];
-$items = [
+$thermometer = [
+    ['class' => 'bg-green', 'text' => 'PX'],
+    ['class' => 'bg-yellow', 'text' => 'BL'],
+    ['class' => 'bg-orange', 'text' => 'NX'],
+    ['class' => 'bg-red', 'text' => 'PZ2A'],    
+];
+
+$stories = [
     [
-        'name' => 'kremas',
-        'size' => 5,
+        'class' => 'text-green',
+        'stories' => [
+            'Atsikėliau', 
+            'Prabudau', 
+            'Išlipau iš lovos'
+        ],
     ],
     [
-        'name' => 'telefonas',
-        'size' => 10,
+        'class' => 'text-yellow',
+        'stories' => [
+            'Vėl pirmadienis', 
+            'Apšalę mašinos langai', 
+            'Nėra ką valgyt pusryčiams'
+        ],
     ],
     [
-        'name' => 'akiniai',
-        'size' => 9,
-    ],
+        'class' => 'text-orange',
+        'stories' => [
+            'Pavėlavau į darbo meetą', 
+            'Pamiršau cigaretes',
+            'Balandis apšiko švarką'
+        ],
+    ],   
     [
-        'name' => 'lupdazis',
-        'size' => 3,
-    ],
-    [
-        'name' => 'vaistai',
-        'size' => 3,
+        'class' => 'text-red',
+        'stories' => [
+            'Užtvindžiau kaimynus', 
+            'Mane užtvindė kaimynai', 
+            'Sumaišiau panaudotą katės kraiką su šokoladu'
+        ],
     ],
 ];
 
-foreach ($items as $item) {
-        $rankinukas[] = $items[rand(0, sizeof($items) - 1)]; 
+$target_state = rand(1, 4);
+
+foreach ($thermometer as $state_idx => $state) {
+    if ($state_idx < $target_state) {
+        // Set Story
+        $story_idx = array_rand($stories[$state_idx]['stories']);        
+        $stories[$state_idx]['chosen_story'] = $stories[$state_idx]['stories'][$story_idx];
+    } else {
+        $thermometer[$state_idx]['class'] = 'white';
+        unset($stories[$state_idx]);
+    }
+
+    if ($state_idx !== $target_state) {
+        $thermometer[$state_idx]['name'] = '';
+    }
 }
 
-$h1_text = 'Ka moteris turi rankinuke?';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Thermometer</title>
+        <style>
+            body {
+                background: black;
+            }
+            
+            .container {
+                display: flex;
+                align-items: center;
+            }
 
-<html>
+            .thermo-circle, .thermo-box {                
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-weight: bold; 
+                color: white;  
+                margin-left: 10px;                
+            }
+            
+            .thermo-circle {
+                width: 100px;
+                height: 100px;
+                background-color: green;
+                border-radius: 100%;            
+            }
+
+            .thermo-box {
+                width: 100px;
+                height: 50px;
+                border: 2px solid grey;                            
+            }
+
+
+            .bg-green {
+                background-color: green;
+            }
+
+            .bg-yellow {
+                background-color: #f3f309;
+            }
+
+            .bg-red {
+                background-color: red;
+            }
+
+            .bg-orange {
+                background-color: orange;
+            }
+
+            .bg-white {
+                background-color: white;
+            }
+            
+            .text-green {
+                color: green;
+            }
+            
+            .text-yellow {
+                color: yellow;
+            }
+            
+            .text-orange {
+                color:  orange;
+            }
+            
+            .text-red {
+                color: red;
+            }
+        </style>
+    </head>
     <body>
-        <h1><?php print $h1_text; ?></h1>
+        <div class="container">
+            <div class="thermo-circle">0</div>
+            
+            <?php foreach ($thermometer as $state_idx => $state) : ?>
+                <div class="thermo-box <?php print $state['class']; ?>">
+                    <?php print $state['text']; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
         <ul>
-            <?php foreach ($rankinukas as $item): ?>
-                <li>
-                    <?php print $item['name']; ?> uzima 
-                    <?php print $item['size']; ?> cm3. 
+            <?php foreach ($stories as $story): ?>
+                <li class="<?php print $story['class']; ?>">
+                    <?php print $story['chosen_story']; ?>
                 </li>
             <?php endforeach; ?>
         </ul>
     </body>
 </html>
-
-
-
-
-
-
