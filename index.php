e5d50b3cb2d178e10c70c0220bb883eac8ee51de
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-24 11:22:18 +0300

    09/24 drinks retail price

--- index.php
+++ index.php
@@ -1,57 +1,40 @@
 <?php
-$natos = [
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'A',
-    'B'
+
+$drinks = [
+    [
+        'name' => 'Vilkmerges Alus',
+        'price_stock' => 3.6,
+        'discount' => 0,
+        'img' => 'url("https://www.barbora.lt/api/Images/GetInventoryImage?id=7acd8bad-f09a-470c-9646-e134ddeee5d7")',
+    ],
+    [
+        'name' => 'Stumbro Degtine',
+        'price_stock' => 3.6,
+        'discount' => 5, //%
+        'img' => 'url("https://www.barbora.lt/api/Images/GetInventoryImage?id=e11360a3-0864-4441-b8da-9cbb8d189742")',
+    ],
+    [
+        'name' => 'Svyturio Ekstra',
+        'price_stock' => 1.6,
+        'discount' => 0, //%
+        'img' => 'url("https://alko-msk.ru/wp-content/uploads/2019/02/ca57b8f0d2fd63ff148960ca99364fb3-67-300x300.png")',
+    ],
+    [
+        'name' => 'Ajax Super Effect',
+        'price_stock' => 2.4,
+        'discount' => 25, //%
+        'img' => 'url("https://www.biurogidas.lt/image/cache/data/catalog/products/oday/117060_1-300x300.png")',
+    ],
 ];
 
-var_dump($natos);
-$natu_suma = count($natos);
+foreach ($drinks as $drink_key => $drink) {
 
-//$root = rand(0, count($natos) - 1);
-var_dump($root);
-
-$akordas = [];
-for ($i = 0; $i < 3; $i++) {
-    $root = rand(0, $natu_suma - 1);
-    for ($j = 0; $j < 3; $j++, $root += 2) {
-        if ($root >= $natu_suma) {
-            $nata = $root - $natu_suma;
-            $akordas[$i][$nata] = $natos[$nata];
-        } else {
-            $nata = $root;
-            $akordas[$i][$nata] = $natos[$nata];
-        }
+    if ($drink['discount'] == 0) {
+        $drinks[$drink_key]['price_retail'] = $drink['price_stock'];
+    } else {
+        $drinks[$drink_key]['price_retail'] = $drink['price_stock'] - ($drink['price_stock'] * $drink['discount'] / 100);
     }
 }
 
-var_dump($akordas);
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Music</title>
-        <style>
-            div {
-                width: 45px;
-                height: 300px;
-                border: 1px solid black;
-                display: inline-block;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="c">C</div>
-        <div class="d">D</div>
-        <div class="e">E</div>
-        <div class="f">F</div>
-        <div class="g">G</div>
-        <div class="a">A</div>
-        <div class="b">B</div>
-    </body>
-</html>
+var_dump($drinks);
+
