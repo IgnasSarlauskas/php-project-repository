01d776f05c7968b2793b9e823a4a9c59f78cb3dc
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-25 13:20:52 +0300

    09/25 #4 slot run array generator fix1

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ function slot_run($size) {
     for ($i = 1; $i <= $size; $i++) {
         for ($j = 1; $j <= $size; $j++) {
            $array[$i][$j] = rand(0, 1);
-           
         }
     }
     return $array;
