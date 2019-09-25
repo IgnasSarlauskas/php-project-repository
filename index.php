0953ff41239d3bf0a5fb686d524596b46288147e
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-25 10:06:25 +0300

    09/25 #1 function declaration

--- index.php
+++ index.php
@@ -1,91 +1,18 @@
 <?php
-$books = [
-    [
-        'title' => 'book 1',
-        'author' => 'author 1',
-        'year' => 2003,
-        'genre' => 'romance'
-    ],
-    [
-        'title' => 'book 2',
-        'author' => 'author 2',
-        'year' => 2008,
-        'genre' => 'biography'
-    ],
-    [
-        'title' => 'book 3',
-        'author' => 'author 3',
-        'year' => 2010,
-        'genre' => 'detective'
-    ],
-    [
-        'title' => 'book 4',
-        'author' => 'author 4',
-        'year' => 1999,
-        'genre' => 'detective'
-    ],
-    [
-        'title' => 'book 5',
-        'author' => 'author 5',
-        'year' => 2001,
-        'genre' => 'classic'
-    ],
-];
 
-var_dump($books);
+function sum($x, $y) {
+    return $x + $y;
+}
+
+$x = 3;
+$y = 4;
+$sum = sum($x, $y);
+$text = "$x ir $y suma: $sum";
+
 ?>
 
 <html>
-    <head>
-        <style>
-            table {
-                font-family: arial, sans-serif;
-                border-collapse: collapse;
-                width: 100%;
-            }
-
-            td, th {
-                border: 1px solid #dddddd;
-                text-align: left;
-                padding: 8px;
-            }
-
-            tr:nth-child(even) {
-                background-color: #dddddd;
-            }
-        </style>
-    </head>
     <body>
-
-        <h2>Books</h2>
-
-        <table>
-            <thead>
-                <?php foreach ($books[0] as $book_idx => $value): ?> 
-                <th><?php print $book_idx; ?></th>
-            <?php endforeach; ?>
-        </thead>
-        <tbody>
-
-            <?php foreach ($books[0] as $book_key => $value): ?>
-                <tr>
-                    <td><?php print $value; ?></td>
-                </tr>
-            <?php endforeach; ?>
-
-            <tr>
-                <?php foreach ($books[1] as $book_key => $value): ?>
-                    <td><?php print $value; ?></td>
-                <?php endforeach; ?>
-            </tr>
-            <tr>
-                <?php foreach ($books[2] as $book_key => $value): ?>
-                    <td><?php print $value; ?></td>
-                <?php endforeach; ?>
-            </tr>
-        </tbody>
-
-    </table>
-
-</body>
+        <h1><?php print $text; ?></h1>
+    </body>
 </html>
