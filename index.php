b3f5a021306d1e313b9f02645fc075f92a7d7a4a
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-25 11:42:03 +0300

    09/25 #2 function is prime or not

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
-function sum($x, $y) {
-    return $x + $y;
+$x = 9;
+
+function is_prime($x) {
+    if ($x == 1) {
+        return 0;
+    } else {
+        for ($i = 2; $i <= $x / 2; $i++) {
+            if ($x % $i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
 }
 
-$x = 3;
-$y = 4;
-$sum = sum($x, $y);
-$text = "$x ir $y suma: $sum";
+if (is_prime($x) == 1) {
+    $text = "Skaicius $x yra pirminis";
+} else {
+    $text = "Skaicius $x nera pirminis";
+}
 
 ?>
+
 <html>
     <body>
         <h1><?php print $text; ?></h1>
