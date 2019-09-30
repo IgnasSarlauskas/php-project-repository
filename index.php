acb83479519952b62ec657eda07861943458fd60
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-09-30 15:02:36 +0300

    09/30 Forms #H1 Duhas Sertificate form

--- index.php
+++ index.php
@@ -1,26 +1,132 @@
 <?php
 
-$answer = 0;
-$image = 'http://www.jackieleonards.ie/wp-content/uploads/2015/03/banana-300x300.png';
-
-function add_one($x) {
-    return $_POST['submit'] + 1;
-}
+$duchai = [];
 
 if (isset($_POST['submit'])) {
-    $answer = add_one($_POST['submit']);
-}
+    foreach ($_POST as $name => $value) {
+        $duchai[$name] = $value;
+    }
+    if ($duchai['submit']) {
+        unset($duchai['submit']);
+    }
 
-var_dump($_POST);
+    $stamp_image = 'https://cttgraphics.com/wp-content/uploads/2018/06/71nOdJPiZkL._SL1000_.jpg';
+    $signature_image = 'https://upload.wikimedia.org/wikipedia/commons/8/80/Joachim_Gaucks_signature.svg';
+
+    $name = $duchai['name'];
+    $surname = $duchai['surname'];
+    $age = $duchai['age'];
+    $level = $duchai['level-list'];
+    $date = date("l jS \of F Y ");
+
+    $h2_text = ' Recognition sertificate for being a Duchas';
+    $h4_text = "This sertificate is presented to $name $surname in Recognition of Valuable Contribution for Duchai Community"
+            . " and also being Duchas";
+    $p_text = "Current level of competency: $level. Achieved at beign $age years old";
+}
 
 ?>
 <html>
+    <head>
+        <title>Ducho anketa</title>
+    </head>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        form {
+            width: 50%;
+            height: 400px;
+            margin: 0 auto;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        input[type=text], input[type=number], select {
+            width: 100%;
+            padding: 15px;
+            margin: 5px 0 22px 0;
+            display: inline-block;
+            border: none;
+            background: #f1f1f1;
+        }
+
+        input[type=text]:focus, input[type=password]:focus {
+            background-color: #ddd;
+            outline: none;
+        }
+
+        input[type=submit] {
+            background-color: black;
+            color: white;
+            padding: 16px 20px;
+            margin: 8px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+            opacity: 0.9;
+        }
+
+        input[type=submit]:hover {
+            opacity: 1;
+        }
+
+        img {
+            margin-top: 30px;
+            display: block;
+            height: 250px;
+            width: 250px;
+            margin: 0 auto;
+        }
+
+        h2{
+            margin-bottom: 50px;
+            margin-top: 40px
+        }
+
+        h4{
+            margin-bottom: 50px;
+        }
+
+        p {
+            margin-bottom: 50px;
+        }
+
+        .signature-container {
+            display: flex;
+            flex-direction: column;
+        }
+
+    </style>
     <body>
-        <form method = "POST">
-            <input name="submit" type ="submit" value="<?php print $answer; ?>"/>
-        </form>
-        <?php for ($i = 0; $i < $answer; $i ++): ?>
-            <img src="<?php print $image; ?>">
-        <?php endfor; ?>
+
+        <?php if (isset($_POST['submit'])): ?>
+            <h2 class="text-center"><?php print $h2_text; ?></h2>
+            <img src="<?php print $stamp_image ?>">
+            <h4 class="text-center"><?php print $h4_text; ?></h4>
+            <p class="text-center"><?php print $p_text; ?></p>
+            <div class="text-center"><?php print $date; ?></div>
+            <img src="<?php print $signature_image ?>">
+        <?php else: ?>
+            <h1 class="text-center">Duchamoro anketa</h1>
+            <form method = "POST">
+                <input type="text" name="name" placeholder="Jusu vardas" required>
+                <input type="text" name="surname" placeholder="Jusu pavarde" required>
+                <input type="number" name="age" placeholder="Jusu amzius">
+                <select name="level-list">
+                    <option value="Pradedantysis" selected>Pradedantysis</option>
+                    <option value="Pazenges">Pazenges</option>
+                    <option value="Profesionalas">Profesionalas</option>
+                </select>
+                <input name="submit" type="submit" />
+            </form>
+        <?php endif; ?>
     </body>
 </html>
