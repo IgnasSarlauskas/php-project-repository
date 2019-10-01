c6974583daa741c04eb814a090b31ec918633380
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-01 12:17:04 +0300

    10/01 #4 modified form.tpl.php + clean function to generate html tags

--- index.php
+++ index.php
@@ -6,17 +6,32 @@ $form = [
         'method' => 'POST',
         'class' => 'my-form',
         'id' => 'login-form'
+    ],
+    'fields' => [
+        [
+            'name' => 'test'
+        ],
+        [
+            'type' => 'test-1'
+        ]
+    ],
+    'buttons' => [
+        [
+            'name' => 'test',
+            'class' => 'test-1',
+        ]
     ]
 ];
 
 function html_attr($attr) {
     foreach ($attr as $key => $value) {
-        $attr_array[] = "$key = \"$value\"";
+        $attr_array[] = strtr('@key="@value"', [
+            '@key' => $key,
+            '@value' => $value
+        ]);
     }
-    return implode(' ',$attr_array);
+    return implode(' ', $attr_array);
 }
-
-var_dump(html_attr($form['attr']));
 
 ?>
 <html>
--- templates/form.tpl.php
+++ templates/form.tpl.php
@@ -1,3 +1,13 @@
 
-<form <?php print html_attr($form['attr']); ?>> </form>
+<form <?php print html_attr(($form['attr'] ?? []) + ['method' => 'POST']); ?>> 
+    <!-- Input Field Generation  -->
+    <?php foreach ($form['fields'] ?? [] as $field_id => $field): ?>
+        <input <?php print html_attr($field); ?>>
+    <?php endforeach; ?>
+    
+    <!-- Button Field Generation  -->
+    <?php foreach ($form['buttons'] ?? [] as $button_id => $button): ?>
+    <button <?php print html_attr($button); ?>> Test</button>
+    <?php endforeach; ?>
+</form>
 
