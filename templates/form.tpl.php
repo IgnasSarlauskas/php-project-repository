7f5bb04c4c27d47ca736f925b3fd3622dfb41524
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-02 15:06:13 +0300

    10/02 #2 Modified field array with attr and extra-attr array + modified input print in form + select multiple choices input.tpl.php final checked by Aurimas

--- templates/form.tpl.php
+++ templates/form.tpl.php
@@ -7,36 +7,36 @@
         <!-- Check if field array has 'label' and print it in case it is true  -->
         <?php if (isset($field['label'])) : ?>
             <label>
-                <?php print $field['label']; ?>
-            <?php endif; ?>
+                <span><?php print $field['label']; ?></span>
+        <?php endif; ?>
 
-            <!-- Check if field array has 'type => select' and print it in case it is true  -->
-            <?php if ($field['attr']['type'] == 'select') : ?>
-                <select <?php print html_attr(['name' => $field_id] + ($field['extra_attr'] ?? [])); ?>>
-                    
-                    <!-- getting option values ant printing them  -->
-                    <?php foreach ($field['options'] as $field_value_id => $field_value): ?>
-                        <option value="<?php print $field_value_id; ?>"><?php print $field_value; ?></option>
-                    <?php endforeach; ?>
-                </select>
-            <?php else: ?>
-                
-                <!--printing inputs with their attributes from form array-->
-                <input <?php print html_attr(['name' => $field_id] + $field['attr'] + ($field['extra_attr'] ?? [])); ?>>
-            <?php endif; ?>
-            
-            <!-- Check For Error message  -->
-            <?php if (isset($field['error'])) : ?>
+        <!-- Check if field array has 'type => select' and print it in case it is true  -->
+        <?php if ($field['attr']['type'] == 'select') : ?>
+            <select <?php print html_attr(['name' => $field_id] + ($field['extra_attr'] ?? [])); ?>>
+
+                <!-- getting option values ant printing them  -->
+                <?php foreach ($field['options'] as $field_value_id => $field_value): ?>
+                    <option value="<?php print $field_value_id; ?>"><?php print $field_value; ?></option>
+                <?php endforeach; ?>
+            </select>
+        <?php else: ?>
+
+            <!--printing inputs with their attributes from form array-->
+            <input <?php print html_attr(['name' => $field_id] + $field['attr'] + ($field['extra_attr'] ?? [])); ?>>
+        <?php endif; ?>
+
+        <!-- Check For Error message  -->
+        <?php if (isset($field['error'])) : ?>
             <div class="color-red"><?php print $field['error'] ?? ''; ?></div>
-            <?php endif; ?>
-            
-            <!-- check if label is set-->
-            <?php if (isset($field['label'])) : ?>
+        <?php endif; ?>
+
+        <!-- check if label is set-->
+        <?php if (isset($field['label'])) : ?>
             </label>
         <?php endif; ?>
         <!--end label check-->
     <?php endforeach; ?>
-        
+
     <!-- Button Field Generation  -->
     <?php foreach ($form['buttons'] ?? [] as $button_id => $button): ?>
         <button <?php print html_attr($button); ?>> <?php print $button_id; ?> </button>
