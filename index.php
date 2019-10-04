0e3e0b19183ea4dd5e063cb363f5146332481ce2
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-04 15:36:08 +0300

    10/04 #2 + #3 validate_form function that calls function from aform array

--- index.php
+++ index.php
@@ -22,6 +22,9 @@ $form = [
             'label' => 'Vardas:',
 //            'error' => 'Vardas per trumpas!',
             'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'validate' => [
+                'validate_not_empty'
+            ]
         ],
         'last_name' => [
             'attr' => [
@@ -107,9 +110,7 @@ function html_attr($attr) {
  * @return array
  */
 function get_filtered_input($form) {
-
     $filter_parameters = [];
-
     foreach ($form['fields'] as $id => $value) {
         if (isset($value['filter'])) {
             $filter_parameters[$id] = $value['filter'];
@@ -117,34 +118,45 @@ function get_filtered_input($form) {
             $filter_parameters[$id] = FILTER_SANITIZE_SPECIAL_CHARS;
         }
     }
-
     return filter_input_array(INPUT_POST, $filter_parameters);
 }
 
-$filtered_input = get_filtered_input($form);
-var_dump($filtered_input);
-
-// All the iput info stays in the field after submitting
-foreach ($form['fields'] as $field_id => &$field) {
-    $field_input = $filtered_input[$field_id];
-    $field['attr']['value'] = $field_input; 
-
-   validate_not_empty($field_input, $field);
-    unset($field); 
-}
 
 /**
- *if input field is empty, error occures above the empty imput field 
+ * if input field is empty, error occures above the empty input field 
  * @param $field_input, &$field
  * @return null | boolean
  */
 function validate_not_empty($field_input, &$field) {
     if ($field_input === '') {
-       $field['error'] = 'Paliktas tuscias laukelis';
-   } else {
-       return true;
-   }
+        $field['error'] = 'Paliktas tuscias laukelis';
+    } else {
+        return true;
+    }
 }
+
+
+/**
+ * function that validates form by checking if input field is empty then error occures above the empty input field 
+ * @param $form
+ * @return null 
+ */
+function validate_form(&$form) {
+    $filtered_input = get_filtered_input($form);
+    // All the iput info stays in the field after submitting
+    foreach ($form['fields'] as $field_id => &$field) {
+        $field_input = $filtered_input[$field_id];
+        $field['attr']['value'] = $field_input;
+        
+        // calling function to check if the field is empty
+            foreach ($field['validate'] ?? [] as $function_name) {
+                $function_name($field_input, $field); // same as => validate_not_empty($field_input, $field);
+            } 
+        unset($field);  
+    }
+}
+
+validate_form($form);
 
 ?>
 <html>
@@ -157,6 +169,6 @@ function validate_not_empty($field_input, &$field) {
         <!--        atprintina nora arba varda jeigu (if ??) _POST mastyve yra noras arba vardas-->
         <h1><?php print $_POST['first_name'] ?? ''; ?></h1>        
         <h2><?php print $_POST['wish'] ?? ''; ?></h2>
-        <?php require 'templates/form.tpl.php'; ?>
+<?php require 'templates/form.tpl.php'; ?>
     </body>
 </html>
